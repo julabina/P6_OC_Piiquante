4f303faa3d58d4f873e518b5cf4d9b86c2b3afc1
Lenfumé Julien <julabina@hotmail.fr>
2022-07-05 15:02:42 +0200

    init + server minimun

---
+++ app.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const morgan = require('morgan');
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+const app = express();
+
+mongoose.connect('mongodb+srv://' + process.env.DB_CONNECT + '@cluster0.9vlpb.mongodb.net/?retryWrites=true&w=majority', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+    .then(() => console.log('Successful database connection'))
+    .catch(() => console.log('Database connection failed'));
+
+app.use(express.json());
+
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next();
+});
+
+app.use(morgan('dev'));
+
+module.exports = app;
